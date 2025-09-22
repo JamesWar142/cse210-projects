402586de5c558c857aa9494a3a428dd649671dc2
JamesDWyatt <wya22003@byui.edu>
2025-09-22 09:15:59 -0600

    9-22-2025

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -22,6 +22,6 @@ class Program
         if (grade >= 70)
         { Console.WriteLine("Congratulations on Passing!"); }
         else
-        { Console.WriteLine("You will pass next time."); }
+        { Console.WriteLine("You will pass next time!"); }
     }
 }
