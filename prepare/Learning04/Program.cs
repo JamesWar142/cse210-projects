b695ec79bbb9735b1c3b4ddbdcf68ebec15cbfa1
JamesDWyatt <wya22003@byui.edu>
2025-10-30 22:13:17 -0600

    Learning04-10/30

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment ment = new Assignment("James Wyatt", "Geometery");
+        Console.WriteLine(ment.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment Mathment = new MathAssignment("Ervin Wyatt", "Addition", "19", "1-10");
+        Console.WriteLine(Mathment.GetSummary());
+        Console.WriteLine(Mathment.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment Writement = new WritingAssignment("Sherry Wyatt", "American History", "The Civil War");
+        Console.WriteLine(Writement.GetSummary());
+        Console.WriteLine(Writement.GetWritingInformation());
     }
 }
